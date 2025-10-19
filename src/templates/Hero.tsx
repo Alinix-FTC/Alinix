573c0a8a3f05f23492a5dd0c51c71c2707c8260a
Cameron-FTC <cameron@futuretechconsulting.com.au>
2025-10-19 14:50:08 +0800

    updates

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -8,7 +8,7 @@ import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
 import { Logo } from './Logo';
 
 const Hero = () => (
-  <Background color="bg-bisque-100">
+  <Background color="bg-#c9c484ff-100">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
@@ -24,13 +24,13 @@ const Hero = () => (
 
     <Section yPadding="pt-20 pb-20">
       <HeroOneButton
-        title="Align - Create - Innovate"
-        description="Understanding your purpose to build your ideas"
-        button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <Button xl>Got a Request?</Button>
-          </Link>
-        }
+      title={<span className="text-#004d66-600">Align - Create - Innovate</span>}
+      description="Understanding your purpose to build your ideas"
+      button={
+        <Link href="https://creativedesignsguru.com/category/nextjs/">
+        <Button xl>Got a Request?</Button>
+        </Link>
+      }
       />
     </Section>
   </Background>
